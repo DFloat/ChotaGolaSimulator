174a51990d6af733aa76ab90b46e3c78819f2372
Appie-1019 <isihyeon005@gmail.com>
2026-07-09 07:43:53 +0900

    [Feat] 새로운 GolaGola 움직임 조작 방식 구현

---
+++ Assets/Script/BodyMovement/GolaGolaBodyPos.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GolaGolaBodyPos : MonoBehaviour
+{
+    public static GolaGolaBodyPos Instance { get; private set; }
+    public static bool canMove = true;
+
+    Vector3 offset;
+    Vector3 originalPos;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (!canMove || MenuPanelToggle.isPanelOpen) return;
+
+        if (DataManager.isMobile) MobileControl();
+        else MouseControl();
+    }
+
+    private void MouseControl()
+    {
+        if (Pointer.current == null) return;
+
+        transform.position = GetPointerPos();
+    }
+
+    public void MobileControl()
+    {
+        if (Pointer.current.press.wasPressedThisFrame)
+        {
+            offset = transform.position - GetPointerPos();
+        }
+
+        if (Pointer.current.press.isPressed)
+        {
+            Vector2 delta = Pointer.current.delta.ReadValue();
+            if (delta.sqrMagnitude > 0.01f)
+            {
+                Vector3 targetPosition = GetPointerPos() + offset;
+                transform.position = Vector3.ClampMagnitude(targetPosition, 20f);
+            }
+        }
+    }
+
+    Vector3 GetPointerPos()
+    {
+        Vector2 mouseScreenPos = Pointer.current.position.ReadValue();
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+
+        mouseWorldPos.z = 0f;
+        return mouseWorldPos;
+    }
+}
