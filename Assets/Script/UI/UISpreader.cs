d6497824cbd4d6ebaf08acef51797124d9892d8f
Appie-1019 <isihyeon005@gmail.com>
2026-07-10 13:59:21 +0900

    [Feat, Fix] 메뉴가 펼쳐질 때, 하위의 모든 메뉴가 접히는 기능 구현
    수직 정렬이 자동으로 되지 않는 문제를 해결하기 위한 기능

--- Assets/Script/UI/UISpreader.cs
+++ Assets/Script/UI/UISpreader.cs
@@ -1,12 +1,14 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(RectTransform))]
 public class UISpreader : MonoBehaviour
 {
-    [Header("Start")]
-    public bool closeAtStart = false;
+    [Header("Setting")]
+    public bool OpenAtStart = false;
+    public float clickCooldown = 0.4f;
     [Header("UI Spread")]
     public RectTransform UIRect;
     public float actDurtaion = 0.2f;
@@ -18,14 +20,20 @@ public class UISpreader : MonoBehaviour
     public float pressedScale = 0.6f;
     public float reductionDuration = 0.15f;
     public float expansionDuration = 0.15f;
+    [Header("Child object")]
+    public UISpreader[] ChildSpreaders;
+    public RectTransform[] extraReducedObject;
 
     RectTransform rect;
     bool isOpen = false;
+    bool lastState = false;
     readonly Vector3 ClosedScale = new Vector3(1, 0, 1);
     readonly Vector3 ClosedRotation = new Vector3(0, 0, 179.999f);
     Vector3 originalSizeDelta;
     Vector3 originalScale;
     Vector3 closeSizeDelta;
+    float[] extraReducedObjectHight;
+    float lastClickTime;
 
     private void Awake()
     {
@@ -43,46 +51,166 @@ public class UISpreader : MonoBehaviour
         closeSizeDelta = UIRect.sizeDelta;
         closeSizeDelta.y = 0;
         originalScale = rect.localScale;
-        SetState(isOpen, true);
+        CollectExtraHeights();
+
+        SetState(OpenAtStart, true);
+        lastState = OpenAtStart;
     }
 
     public void Toggle()
     {
-        SetState(!isOpen);
+        if (Time.time - lastClickTime >= clickCooldown)
+        {
+            lastClickTime = Time.time;
+
+            lastState = !isOpen;
+            SetState(lastState);
+        }
     }
 
-    void SetState(bool enable, bool instant = false)
+    void SetState(bool enable, bool instant = false, float timeReduction = 0)
     {
         UIRect.DOKill();
         rect.DOKill();
         iconImage.DOKill();
 
-        Vector3 targetScale = enable ? Vector3.one : ClosedScale;
-        Vector3 targetSizeDelta = enable ? originalSizeDelta : closeSizeDelta;
+        Vector3 targetScale;
+        Vector3 targetRotation;
+        Color targetColor;
+        Vector3 targetSizeDelta;
+
+        if (enable)
+        {
+            targetScale = Vector3.one;
+            targetRotation = Vector3.zero;
+            targetColor = openColor;
+            targetSizeDelta = originalSizeDelta;
+            RestoreExtraObjectsHeight(instant);
+
+        }
+        else
+        {
+            targetScale = ClosedScale;
+            targetRotation = ClosedRotation;
+            targetColor = closeColor;
+            targetSizeDelta = closeSizeDelta;
+            SetExtraObjectsHeightsToZero(instant, 0.1f);
+        }
+
+        StartCoroutine(ForceChildContorl(enable));
+
 
         if (instant)
         {
             UIRect.localScale = targetScale;
             UIRect.sizeDelta = targetSizeDelta;
+            
+            rect.rotation = Quaternion.Euler(targetRotation);
+
+            iconImage.color = targetColor;
         }
         else
         {
-            UIRect.DOScale(targetScale, actDurtaion).SetEase(Ease.OutQuad);
-            UIRect.DOSizeDelta(targetSizeDelta, actDurtaion).SetEase(Ease.OutQuad);
+            float currentActDuration = actDurtaion - timeReduction;
+            UIRect.DOScale(targetScale, currentActDuration).SetEase(Ease.OutQuad);
+            UIRect.DOSizeDelta(targetSizeDelta, currentActDuration).SetEase(Ease.OutQuad).OnComplete(() => { UIRect.sizeDelta += Vector2.one * 0.005f; });
+
+            rect.DORotate(targetRotation, currentActDuration).SetEase(Ease.OutQuad);
+
+            iconImage.DOColor(targetColor, currentActDuration);
         }
         
-        Vector3 targetRotation = enable ? Vector3.zero : ClosedRotation;
-        if (instant) rect.rotation = Quaternion.Euler(targetRotation);
-        else rect.DORotate(targetRotation, actDurtaion).SetEase(Ease.OutQuad);
-
-        Color targetColor = enable ? openColor : closeColor;
-        if (instant) iconImage.color = targetColor;
-        else iconImage.DOColor(targetColor, actDurtaion);
-
         Sequence seq = DOTween.Sequence();
         seq.Append(rect.DOScale(originalScale * pressedScale, reductionDuration));
         seq.Append(rect.DOScale(originalScale, expansionDuration));
 
         isOpen = enable;
+    }
+
+    IEnumerator ForceChildContorl(bool isOpen)
+    {
+        foreach (UISpreader sp in ChildSpreaders)
+        {
+            if (isOpen)
+            {
+                yield return new WaitForSeconds(0.075f);
+                sp.ForceOpen();
+            }
+            else sp.ForceClose();
+        }
+    }
+
+    public void ForceOpen()
+    {
+        if (lastState != isOpen)
+        {
+            SetState(lastState);
+            lastClickTime = Time.time;
+        }
+    }
+    public void ForceClose()
+    {
+        if (lastState)
+        {
+            SetState(false);
+            lastClickTime = Time.time;
+        }
+    }
+
+    void CollectExtraHeights()
+    {
+        if (extraReducedObject == null || extraReducedObject.Length == 0)
+        {
+            extraReducedObjectHight = new float[0];
+            return;
+        }
+
+        extraReducedObjectHight = new float[extraReducedObject.Length];
+
+        for (int i = 0; i < extraReducedObject.Length; i++)
+        {
+            if (extraReducedObject[i] != null)
+            {
+                extraReducedObjectHight[i] = extraReducedObject[i].sizeDelta.y;
+            }
+            else
+            {
+                extraReducedObjectHight[i] = 0f;
+            }
+        }
+    }
+
+    public void SetExtraObjectsHeightsToZero(bool instant = false, float timeReduction = 0)
+    {
+        if (extraReducedObject == null) return;
+
+        for (int i = 0; i < extraReducedObject.Length; i++)
+        {
+            if (extraReducedObject[i] != null)
+            {
+                Vector2 currentSize = extraReducedObject[i].sizeDelta;
+                currentSize.y = 0f;
+                if (instant) extraReducedObject[i].sizeDelta = currentSize;
+                else extraReducedObject[i].DOSizeDelta(currentSize, actDurtaion - timeReduction);
+            }
+        }
+    }
+
+    public void RestoreExtraObjectsHeight(bool instant = false, float timeReduction = 0)
+    {
+        if (extraReducedObject == null || extraReducedObjectHight == null) return;
+
+        int loopCount = Mathf.Min(extraReducedObject.Length, extraReducedObjectHight.Length);
+
+        for (int i = 0; i < loopCount; i++)
+        {
+            if (extraReducedObject[i] != null)
+            {
+                Vector2 currentSize = extraReducedObject[i].sizeDelta;
+                currentSize.y = extraReducedObjectHight[i];
+                if (instant) extraReducedObject[i].sizeDelta = currentSize;
+                else extraReducedObject[i].DOSizeDelta(currentSize, actDurtaion - timeReduction);
+            }
+        }
     }
 }
